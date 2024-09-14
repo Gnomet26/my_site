528dcc02ba9a781d69dc949a3d9d3babe2f66fad
gnomet026 <gnometerminal026@gmail.com>
2024-09-14 09:35:10 +0500

    going...

--- templates/template_scripts/script_my_favourite_music.js
+++ templates/template_scripts/script_my_favourite_music.js
@@ -1,3 +1,13 @@
+function play_pause_music(this_button){
+
+    if(this_button.className === 'play_and_download'){
+        this_button.classList.remove('play_and_download');
+        this_button.classList.add('pause');
+    }else if(this_button.className === 'pause'){
+        this_button.classList.remove('pause');
+        this_button.classList.add('play_and_download');
+    }
+}
 
 function messagebox(buttonID){
 
