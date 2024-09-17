5df347f413ceaf110610bd381be38c554ec7a2a9
gnomet026 <gnometerminal026@gmail.com>
2024-09-17 17:34:04 +0500

    working...

--- templates/template_scripts/script_create_account_view.js
+++ templates/template_scripts/script_create_account_view.js
@@ -1,20 +1,43 @@
 const question = document.getElementById('question');
+const question_num = 0;
 
 function random_generator(){
     let num = Math.floor(Math.random() * 8) + 1;
-    
     question.className = '';
     const new_class = `for_q${num}`;
     question.classList.add(new_class);
+    question_num = num;
 }
 
 function create_account_f(){
-    const user_name_input = document.getElementById('user_name');
-    const user_password_input = document.getElementById('user_password');
-    const user_repeat_password = document.getElementById('repeat_password');
-    const user_email = document.getElementById('user_email');
-    const answer = document.getElementById('answer_');
+    const user_name_input = String(document.getElementById('user_name').value).split(' ').join('');
+    const user_password_input = String(document.getElementById('user_password').value).split(' ').join('');
+    const user_repeat_password = String(document.getElementById('repeat_password').value).split(' ').join('');
+    const user_email = String(document.getElementById('user_email').value).split(' ').join('');
+    const answer = String(document.getElementById('answer_').value).split(' ').join('');
 
-    
+
+    //alert("Funksiya yaxshi ishlayapti :) Ok"+ user_name_input+" # "+user_password_input+" # "+user_repeat_password+" # "+user_email+" # "+answer);
+
+    if(user_name_input != '' && user_password_input != '' && user_repeat_password != '' && user_email != '' && answer != ''){
+        alert("Ajoyib");
+        // Go ...
+    }
+    else if(user_name_input == ''){
+        alert("Fill in the Usernane field too!")
+    }
+    else if(user_password_input == ''){
+        alert("Password is required")
+    }
+    else if(user_repeat_password == ''){
+        alert("Password is required")
+    }
+    else if(user_email == ''){
+        alert("gmail is required")
+    }
+    else if(answer == ''){
+        alert("answer is required")
+    }
+
 
 }
--- templates/template_scripts/script_login_view.js
+++ templates/template_scripts/script_login_view.js
@@ -1,5 +1,32 @@
+const question = document.getElementById('question');
+const question_num = 0;
 
 function random_generator(){
-    const num = Math.floor(Math.random() * 8) + 1;
-    alert("tasodifiy sonimiz: " + num);
+    let num = Math.floor(Math.random() * 8) + 1;
+    question.className = '';
+    const new_class = `for_q${num}`;
+    question.classList.add(new_class);
+    question_num = num;
+}
+
+function log_in_f(){
+    const user_name_input = String(document.getElementById('name_n_input').value).split(' ').join('');
+    const user_password_input = String(document.getElementById('parol_n_input').value).split('').join('');
+    const answer = String(document.getElementById('answer').value).split(' ').join('');
+
+    //alert("Assalomu aleykum  " + user_name_input+"   "+user_password_input +" " +answer);
+
+    if(user_name_input != '' && user_password_input != '' && answer != ''){
+        alert("Ajoyib");
+        //Go ...
+    }
+    else if(user_name_input == ''){
+        alert("Fill in the Usernane field too!")
+    }
+    else if(user_password_input == ''){
+        alert("Password is required")
+    }
+    else if(answer == ''){
+        alert("answer is required")
+    }
 }
